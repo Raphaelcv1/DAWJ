94b314e6d0978a865544073a5c38c567e63d580d
Raphaelcv1 <raphaelcv1@hotmail.com>
2021-03-22 23:51:57 -0300

    Atividade 2 - Pessoas - Alter and Delete

--- atividade2/src/main/java/br/com/rapha/atividade2/atividade2/controle/PessoaControle.java
+++ atividade2/src/main/java/br/com/rapha/atividade2/atividade2/controle/PessoaControle.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sun.el.stream.Optional;
 
 import br.com.rapha.atividade2.atividade2.modelo.Pessoa;
 import br.com.rapha.atividade2.atividade2.modelo.PessoaRepository;
@@ -29,9 +32,29 @@ public class PessoaControle {
 		return "pessoas/form";
 	}
 	
+	@GetMapping("/pessoas/{id}")
+	public String alterarPessoa(@PathVariable("id") long id, Model model) {
+		java.util.Optional<Pessoa> pessoaOpt =  pessoaRepo.findById(id);
+		if(pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessao inválida");
+		}
+		model.addAttribute("pessoa", pessoaOpt.get());
+		return "pessoas/form";
+	}
+	
 	@PostMapping("/pessoas/salvar")
 	public String salvarPessoa(@ModelAttribute("pessoa") Pessoa pessoa) {
 		pessoaRepo.save(pessoa);
+		return "redirect:/pessoas";
+	}
+	
+	@GetMapping("/pessoas/excluir/{id}")
+	public String excluirPessoa(@PathVariable("id") long id) {
+		java.util.Optional<Pessoa> pessoaOpt =  pessoaRepo.findById(id);
+		if(pessoaOpt.isEmpty()) {
+			throw new IllegalArgumentException("Pessao inválida");
+		}
+		pessoaRepo.delete(pessoaOpt.get());
 		return "redirect:/pessoas";
 	}
 	
